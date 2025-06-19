f985ceb2d67dc0932a8143eea3e47864f96f28f5
Rick South <rick@natamus.com>
2025-06-19 03:46:40 +0200

    Mineral Chance source code for 1.21.6.

---
+++ Common/src/main/java/com/natamus/mineralchance/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.mineralchance;
+
+import com.natamus.mineralchance.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
