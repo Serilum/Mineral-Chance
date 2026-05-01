828a303bc3d783b089f3315ba5edabf85fda9116
Rick South <rick@natamus.com>
2026-05-01 23:49:13 +0200

    (v3.9) (26.1.2 NeoForge) Updated to work with NeoForge version 26.1.2.21-beta and above.

--- NeoForge/src/main/java/com/natamus/mineralchance/neoforge/events/NeoForgeMiningEvent.java
+++ NeoForge/src/main/java/com/natamus/mineralchance/neoforge/events/NeoForgeMiningEvent.java
@@ -3,12 +3,12 @@ package com.natamus.mineralchance.neoforge.events;
 import com.natamus.collective.functions.WorldFunctions;
 import com.natamus.mineralchance.events.MiningEvent;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.event.level.BlockEvent;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.level.block.BreakBlockEvent;
 
 public class NeoForgeMiningEvent {
 	@SubscribeEvent
-	public static void onBlockBreak(BlockEvent.BreakEvent e) {
+	public static void onBlockBreak(BreakBlockEvent e) {
 		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
 		if (level == null) {
 			return;
